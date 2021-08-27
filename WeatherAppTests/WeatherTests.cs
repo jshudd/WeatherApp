dfe00baed5e26568206b6131a74fc87c4fd8b85c
Jeremy Huddleston <jshudd@gmail.com>
2021-08-27 14:28:43 -0500

    test commit
    added some tests

---
+++ WeatherAppTests/WeatherTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using WeatherApp;
+using Xunit;
+
+namespace WeatherAppTests
+{
+    public class WeatherTests
+    {
+        [Theory]
+        [InlineData("35242")]
+        [InlineData("35226")]
+        [InlineData("32541")]
+        [InlineData("37211")]
+        [InlineData("35184")]
+        [InlineData("95014")]
+        public void ShouldGetResponse(string zipCode)
+        {
+            var weather = new Weather();
+
+            weather.GetAPIResponse(zipCode);
+
+            Assert.NotNull(weather.APIResponse);
+        }
+
+        [Theory]
+        [InlineData("35242")]
+        [InlineData("35226")]
+        [InlineData("32541")]
+        [InlineData("37211")]
+        [InlineData("35184")]
+        [InlineData("95014")]
+        public void ShouldParseZip(string zipCode)
+        {
+            Weather weather = new Weather();
+
+            weather.GetAPIResponse(zipCode);
+
+            weather.GetTemp();
+
+            Assert.NotNull(weather.Temperature);
+        }
+
+        [Theory]
+        [InlineData("35242", "Birmingham")]
+        [InlineData("37211", "Nashville")]
+        [InlineData("32541", "Destin")]
+        [InlineData("95014", "Cupertino")]
+        [InlineData("35184", "West Blocton")]
+        public void ShouldParseCity(string zipCode, string expected)
+        {
+            Weather weather = new Weather();
+
+            weather.GetAPIResponse(zipCode);
+
+            weather.GetCityName();
+
+            Assert.Equal(expected, weather.CityName);
+        }
+    }
+}
